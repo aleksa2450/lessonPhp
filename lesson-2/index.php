1536dd4de117fc778f97055ce55343f1ae1b9f39
aleksa2450 <aleksa2450@yandex.ru>
2022-01-17 18:40:55 +0300

    1.4 task

--- lesson-2/index.php
+++ lesson-2/index.php
@@ -3,8 +3,12 @@
 // 1. Дана строка 'hello'. Сделайте из нее строку 'HELLO'.
 
 $stringOne = 'hello';
+
+// 1 вариант
+
 echo strtoupper($stringOne) . PHP_EOL;
-//
+
+// 2 вариант
 
 $stringHello = (strtoupper($stringOne));
 echo "{$stringHello}" . PHP_EOL;
@@ -12,9 +16,11 @@ echo "{$stringHello}" . PHP_EOL;
 // 2. Дана строка 'HELLO'. Сделайте из нее строку 'hello'.
 
 $stringTwo = 'HELLO';
+// 1 вариант
+
 echo (strtolower($stringTwo)) . PHP_EOL;
 
-//
+// 2 вариант
 
 $stringHello = strtolower($stringTwo);
 echo "{$stringHello}" . PHP_EOL;
@@ -22,11 +28,24 @@ echo "{$stringHello}" . PHP_EOL;
 //3. Дана строка 'hello'. Сделайте из нее строку 'Hello'.
 
 $string = 'hello';
+
+// 1 вариант
+
 $stringH =  ucfirst($string);
 echo "{$stringH}" . PHP_EOL;
 
-//
+// 2 вариант
 
 echo ucfirst($string) . PHP_EOL;
 
-//
+//Дана строка 'Hello'. Сделайте из нее строку 'hello'.
+$string = 'Hello1';
+
+//1 вариант
+
+$strh = lcfirst($string);
+echo "{$strh}" . PHP_EOL;
+
+//2 вариант
+
+echo lcfirst($string) . PHP_EOL;
