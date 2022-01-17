6b1393845d948df5b9926beb80fe0781de6f100b
aleksa2450 <aleksa2450@yandex.ru>
2022-01-17 18:24:08 +0300

    1.1 task

---
+++ lesson-2/index.php
@@ -0,0 +1,10 @@
+<?php
+
+//1. Дана строка 'hello'. Сделайте из нее строку 'HELLO'.
+
+$string = 'hello';
+echo strtoupper($string) . PHP_EOL;
+//
+$stringHello = (strtoupper($string));
+echo "{$stringHello}" . PHP_EOL;
+
