4d40a54a0bd32883f768f671bbbfff333f97bf65
aleksa2450 <aleksa2450@yandex.ru>
2022-03-13 10:55:09 +0300

    Пагниация. Все части разбиты по функциям(Кроме подключения)

--- pagination/function.php
+++ pagination/function.php
@@ -35,3 +35,25 @@ function displayRecordsFromDB($RecordsFromDB)
         $i++;
     }
 }
+
+function showNavigatingPages($currentPage, $countPage)
+{
+    //Левая стрелка
+    if ($currentPage != 1) {
+        $prev = $currentPage - 1;
+        echo "<a href=\"?page={$prev}\" style=\"border:1px solid #ccc; padding:5px;\"> <<< </a> ";
+    }
+    //отображение цифр
+    for ($i = 1; $i <= $countPage; $i++) {
+        if ($currentPage == $i) {
+            $class = ' style="border:1px solid #ccc;font-weight:bold;color:red;padding:5px;"';
+        } else {
+            $class = ' style="border:1px solid #ccc;padding:5px"';
+        }
+        echo "<a href=\"?page={$i}\"{$class}>{$i}</a> ";
+    }
+    //правая стрелка
+    $prev = ($currentPage != $countPage) ? $currentPage + 1 : $currentPage;
+    echo "<a href=\"?page={$prev}\" style=\"border:1px solid #ccc;padding:5px\"> >>> </a>";
+    echo '<div style="height:200px;"></div>';
+}
--- pagination/index.php
+++ pagination/index.php
@@ -30,22 +30,6 @@ $RecordsFromDB = getRecordsFromDB($dbh, $limit, $perPage);
 
 displayRecordsFromDB($RecordsFromDB);
 
-if ($currentPage != 1) {
-    $prev = $currentPage - 1;
-    echo "<a href=\"?page={$prev}\" style=\"border:1px solid #ccc; padding:5px;\"> <<< </a> ";
-}
-for ($i = 1; $i <= $countPage; $i++) {
-    if ($currentPage == $i) {
-        $class = ' style="border:1px solid #ccc;font-weight:bold;color:red;padding:5px;"';
-    } else {
-        $class = ' style="border:1px solid #ccc;padding:5px"';
-    }
-    echo "<a href=\"?page={$i}\"{$class}>{$i}</a> ";
-}
-$prev = ($currentPage != $countPage) ? $currentPage + 1 : $currentPage;
-echo "<a href=\"?page={$prev}\" style=\"border:1px solid #ccc;padding:5px\"> >>> </a>";
-echo '<div style="height:200px;"></div>';
-
-
+showNavigatingPages($currentPage, $countPage);
 
 
