18c23ea1b1b0034afd0dccf14baab49b08bef0a7
aleksa2450 <aleksa2450@yandex.ru>
2022-03-14 18:51:24 +0300

    UPDATE

---
+++ PDO/update.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+error_reporting(-1);
+
+define('DSN', [
+    'dsn' => 'mysql:localhost=localhost;dbname=pdo;charset=utf8',
+    'username' => 'root',
+    'password' => '',
+]);
+
+define('OPTIONS', [
+    \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+    \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"
+]);
+
+try {
+    $dbh = new \PDO(
+        DSN['dsn'],
+        DSN['username'],
+        DSN['password'],
+        OPTIONS
+    );
+} catch (\PDOException $e) {
+    echo $e->getMessage();
+}
+
+$sth = $dbh->prepare("UPDATE products SET price = 11111 where price = 0");
+$sth->execute();
+
+$query = "UPDATE products SET price = 0 WHERE price = 11111";
+$dbh->query($query);
